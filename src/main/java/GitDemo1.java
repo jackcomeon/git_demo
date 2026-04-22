dd174f743e5d5a88b712b8fbf4703cbaa2927a5c
liujunfan <2936152036@qq.com>
2026-04-22 21:50:11 +0800

    第一次提交

---
+++ src/main/java/GitDemo1.java
@@ -0,0 +1,11 @@
+/**
+ * @author liujunfan
+ * @project IntelliJ IDEA
+ * @data 2026/4/22
+ **/
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
